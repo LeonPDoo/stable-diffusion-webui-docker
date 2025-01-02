cfdc8e6ae4e5a3142574a812472689ea0c9c35e0
LeonPDoo <leon.petersohn@doopic.com>
2025-01-02 14:24:41 +0100

    Update download.sh

--- services/download/download.sh
+++ services/download/download.sh
@@ -1,30 +1,9 @@
 #!/usr/bin/env bash
 
-set -Eeuo pipefail
+wget "https://huggingface.co/LeonP/VF/resolve/main/flux1-dev-fp8.safetensors" -O unet/flux1-dev-fp8.safetensors
 
-# TODO: maybe just use the .gitignore file to create all of these
-mkdir -vp /data/.cache \
-  /data/embeddings \
-  /data/config/ \
-  /data/models/ \
-  /data/models/Stable-diffusion \
-  /data/models/GFPGAN \
-  /data/models/RealESRGAN \
-  /data/models/LDSR \
-  /data/models/VAE
+wget "https://huggingface.co/LeonP/VF/resolve/main/ae.safetensors" -O VAE/ae.safetensors
 
-echo "Downloading, this might take a while..."
+wget "https://huggingface.co/comfyanonymous/flux_text_encoders/resolve/main/t5xxl_fp16.safetensors" -O clip/t5xxl_fp16.safetensors
 
-aria2c -x 10 --disable-ipv6 --input-file /docker/links.txt --dir /data/models --continue
-
-#echo "Checking SHAs..."
-#parallel --will-cite -a /docker/checksums.sha256 "echo -n {} | sha256sum -c"
-
-cat <<EOF
-By using this software, you agree to the following licenses:
-https://github.com/AbdBarho/stable-diffusion-webui-docker/blob/master/LICENSE
-https://github.com/CompVis/stable-diffusion/blob/main/LICENSE
-https://github.com/AUTOMATIC1111/stable-diffusion-webui/blob/master/LICENSE.txt
-https://github.com/invoke-ai/InvokeAI/blob/main/LICENSE
-And licenses of all UIs, third party libraries, and extensions.
-EOF
+wget "https://huggingface.co/comfyanonymous/flux_text_encoders/resolve/main/clip_l.safetensors" -O clip/clip_l.safetensors
