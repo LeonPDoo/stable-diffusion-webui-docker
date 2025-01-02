7b5c5c939eec3d96902b664b81ceea718a770438
LeonPDoo <leon.petersohn@doopic.com>
2025-01-02 16:32:16 +0100

    Update download.sh

--- services/download/download.sh
+++ services/download/download.sh
@@ -15,7 +15,18 @@ mkdir -vp /data/.cache \
 
 echo "Downloading, this might take a while..."
 
-aria2c -x 10 --disable-ipv6 --input-file /docker/links.txt --dir /data/models --continue
+#aria2c -x 10 --disable-ipv6 --input-file /docker/links.txt --dir /data/models --continue
+
+mkdir /data/models/unet
+wget "https://huggingface.co/LeonP/VF/resolve/main/flux1-dev-fp8.safetensors" -O /data/models/unet/flux1-dev-fp8.safetensors
+
+mkdir /data/models/VAR
+wget "https://huggingface.co/LeonP/VF/resolve/main/ae.safetensors" -O /data/models/VAE/ae.safetensors
+
+mkdir /data/models/clip
+wget "https://huggingface.co/comfyanonymous/flux_text_encoders/resolve/main/t5xxl_fp16.safetensors" -O /data/models/clip/t5xxl_fp16.safetensors
+
+wget "https://huggingface.co/comfyanonymous/flux_text_encoders/resolve/main/clip_l.safetensors" -O /data/models/clip/clip_l.safetensors
 
 #echo "Checking SHAs..."
 #parallel --will-cite -a /docker/checksums.sha256 "echo -n {} | sha256sum -c"
