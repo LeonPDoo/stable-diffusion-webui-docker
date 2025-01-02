0b41ecef698dc17aedcc76ee0f361d5631531f99
LeonPDoo <leon.petersohn@doopic.com>
2025-01-02 14:27:00 +0100

    Update download.sh

--- services/download/download.sh
+++ services/download/download.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
+mkdir unet
 wget "https://huggingface.co/LeonP/VF/resolve/main/flux1-dev-fp8.safetensors" -O unet/flux1-dev-fp8.safetensors
 
+mkdir VAR
 wget "https://huggingface.co/LeonP/VF/resolve/main/ae.safetensors" -O VAE/ae.safetensors
 
+mkdir clip
 wget "https://huggingface.co/comfyanonymous/flux_text_encoders/resolve/main/t5xxl_fp16.safetensors" -O clip/t5xxl_fp16.safetensors
 
 wget "https://huggingface.co/comfyanonymous/flux_text_encoders/resolve/main/clip_l.safetensors" -O clip/clip_l.safetensors
