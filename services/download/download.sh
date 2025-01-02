e099d2b004872fb11976eb694b6423c8a92542dc
LeonPDoo <leon.petersohn@doopic.com>
2025-01-02 16:37:11 +0100

    Update download.sh

--- services/download/download.sh
+++ services/download/download.sh
@@ -13,16 +13,16 @@ echo "Downloading Models, this might take a while..."
 
 #aria2c -x 10 --disable-ipv6 --input-file /docker/links.txt --dir /data/models --continue
 
-mkdir /data/models/unet
-wget "https://huggingface.co/LeonP/VF/resolve/main/flux1-dev-fp8.safetensors" -O /data/models/unet/flux1-dev-fp8.safetensors
+#mkdir /data/models/unet
+#wget "https://huggingface.co/LeonP/VF/resolve/main/flux1-dev-fp8.safetensors" -O /data/models/unet/flux1-dev-fp8.safetensors
 
 mkdir /data/models/VAE
 wget "https://huggingface.co/LeonP/VF/resolve/main/ae.safetensors" -O /data/models/VAE/ae.safetensors
 
-mkdir /data/models/clip
-wget "https://huggingface.co/comfyanonymous/flux_text_encoders/resolve/main/t5xxl_fp16.safetensors" -O /data/models/clip/t5xxl_fp16.safetensors
+#mkdir /data/models/clip
+#wget "https://huggingface.co/comfyanonymous/flux_text_encoders/resolve/main/t5xxl_fp16.safetensors" -O /data/models/clip/t5xxl_fp16.safetensors
 
-wget "https://huggingface.co/comfyanonymous/flux_text_encoders/resolve/main/clip_l.safetensors" -O /data/models/clip/clip_l.safetensors
+#wget "https://huggingface.co/comfyanonymous/flux_text_encoders/resolve/main/clip_l.safetensors" -O /data/models/clip/clip_l.safetensors
 
 
 # Custom Nodes:
